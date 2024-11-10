dc1de6b645a8ed958b0e0cba91cd69fa809c97e1
Joemar Palting <joemarpalting19@gmail.com>
2024-11-10 12:06:13 +0800

    Added vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import "./assets/tailwind.css";
 import "aos/dist/aos.css";
-
+import { inject } from "@vercel/analytics";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+inject();
 
 const app = createApp(App);
 
